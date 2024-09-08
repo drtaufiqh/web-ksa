6e1412031d254c0f33f8bf1c912564a202164d2f
Dutatama Rosewika Taufiq Hadihardaya <dutarosewika@gmail.com>
2024-09-08 17:29:10 +0700

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,8 @@ Route::get('/dashboard', [PadiAmatanController::class, 'showDashboard'])->middle
 // login logout
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login')->middleware("guest");
 Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
 // padi
 
@@ -52,11 +52,11 @@ Route::get('/unduh-padi', [GambarController::class, 'unduhPadi'])->middleware('a
 
 // padi_unggah
 Route::get('/padi_unggah', [PadiAmatanController::class, 'showUploadForm'])->middleware('auth')->name('padi_unggah');
-Route::post('/padiamatan/upload', [PadiAmatanController::class, 'import'])->name('padiamatan.upload');
+Route::post('/padiamatan/upload', [PadiAmatanController::class, 'import'])->name('padiamatan.upload')->middleware('auth');
 
 // padi_riwayat
 Route::get('/padi_riwayat', [PadiAmatanController::class, 'riwayat'])->middleware('auth')->name('padi_riwayat');
-Route::get('/padi_detail/{id}/{tahun}/{bulan}', [PadiAmatanController::class, 'showDetail']);
+Route::get('/padi_detail/{id}/{tahun}/{bulan}', [PadiAmatanController::class, 'showDetail'])->middleware('auth');
 
 // padi_validasi
 Route::get('/padi_validasi', [PadiValidasiController::class, 'showValidasi'])->middleware('auth')->name('padi_validasi');
@@ -68,15 +68,15 @@ Route::get('/padi_panduan', function () {
     return view('padi.panduan');
 })->middleware('auth')->name('padi_panduan');
 
-Route::post('/padi-get-data-peta', [PadiAmatanController::class, 'getDataPeta'])->name('padi.get.data.peta');
-Route::get('/padi-get-data-peta-sebaran', [PadiAmatanController::class, 'getDataPetaSebaran'])->name('padi.get.data.peta.sebaran');
-Route::post('/padi-get-data-peta-sebaran', [PadiAmatanController::class, 'getDataPetaSebaran'])->name('padi.get.data.peta.sebaran');
-Route::get('/padi-get-data-capaian', [PadiAmatanController::class, 'getDataCapaian']);
-Route::post('/padi-get-data-progres', [PadiAmatanController::class, 'getDataProgres'])->name('padi.get.data.progres');
+Route::post('/padi-get-data-peta', [PadiAmatanController::class, 'getDataPeta'])->middleware('auth')->name('padi.get.data.peta');
+Route::get('/padi-get-data-peta-sebaran', [PadiAmatanController::class, 'getDataPetaSebaran'])->middleware('auth')->name('padi.get.data.peta.sebaran');
+Route::post('/padi-get-data-peta-sebaran', [PadiAmatanController::class, 'getDataPetaSebaran'])->middleware('auth')->name('padi.get.data.peta.sebaran');
+Route::get('/padi-get-data-capaian', [PadiAmatanController::class, 'getDataCapaian'])->middleware('auth');
+Route::post('/padi-get-data-progres', [PadiAmatanController::class, 'getDataProgres'])->middleware('auth')->name('padi.get.data.progres');
 
 // testing only
-Route::get('/test-proses', [PadiAmatanController::class, 'testProses'])->name('test.proses');
-Route::post('/test-proses', [PadiAmatanController::class, 'runProses'])->name('run.proses');
+Route::get('/test-proses', [PadiAmatanController::class, 'testProses'])->name('test.proses')->middleware('auth');
+Route::post('/test-proses', [PadiAmatanController::class, 'runProses'])->name('run.proses')->middleware('auth');
 
 // jagung
 
@@ -91,11 +91,11 @@ Route::get('/unduh-jagung', [GambarController::class, 'unduhJagung'])->middlewar
 
 // jagung_unggah
 Route::get('/jagung_unggah', [JagungAmatanController::class, 'showUploadForm'])->middleware('auth')->name('jagung_unggah');
-Route::post('/jagungamatan/upload', [JagungAmatanController::class, 'import'])->name('jagungamatan.upload');
+Route::post('/jagungamatan/upload', [JagungAmatanController::class, 'import'])->middleware('auth')->name('jagungamatan.upload');
 
 // jagung_riwayat
 Route::get('/jagung_riwayat', [JagungAmatanController::class, 'riwayat'])->middleware('auth')->name('jagung_riwayat');
-Route::get('/jagung_detail/{id}/{tahun}/{bulan}', [JagungAmatanController::class, 'showDetail']);
+Route::get('/jagung_detail/{id}/{tahun}/{bulan}', [JagungAmatanController::class, 'showDetail'])->middleware('auth');
 
 // jagung_validasi
 Route::get('/jagung_validasi', [JagungValidasiController::class, 'showValidasi'])->middleware('auth')->name('jagung_validasi');
@@ -108,37 +108,37 @@ Route::get('/jagung_panduan', function () {
 })->middleware('auth')->name('jagung_panduan');
 
 
-Route::post('/jagung-get-data-peta', [JagungAmatanController::class, 'getDataPeta'])->name('jagung.get.data.peta');
-Route::get('/jagung-get-data-peta-sebaran', [JagungAmatanController::class, 'getDataPetaSebaran']);
-Route::post('/jagung-get-data-peta-sebaran', [JagungAmatanController::class, 'getDataPetaSebaran'])->name('jagung.get.data.peta.sebaran');
-Route::get('/jagung-get-data-capaian', [JagungAmatanController::class, 'getDataCapaian']);
-Route::post('/jagung-get-data-progres', [JagungAmatanController::class, 'getDataProgres'])->name('jagung.get.data.progres');
-Route::get('/jagung-get-data-progres', [JagungAmatanController::class, 'getDataProgres'])->name('jagung.get.data.progres');
+Route::post('/jagung-get-data-peta', [JagungAmatanController::class, 'getDataPeta'])->middleware('auth')->name('jagung.get.data.peta');
+Route::get('/jagung-get-data-peta-sebaran', [JagungAmatanController::class, 'getDataPetaSebaran'])->middleware('auth');
+Route::post('/jagung-get-data-peta-sebaran', [JagungAmatanController::class, 'getDataPetaSebaran'])->middleware('auth')->name('jagung.get.data.peta.sebaran');
+Route::get('/jagung-get-data-capaian', [JagungAmatanController::class, 'getDataCapaian'])->middleware('auth');
+Route::post('/jagung-get-data-progres', [JagungAmatanController::class, 'getDataProgres'])->middleware('auth')->name('jagung.get.data.progres');
+Route::get('/jagung-get-data-progres', [JagungAmatanController::class, 'getDataProgres'])->middleware('auth')->name('jagung.get.data.progres');
 
 // ubinan
 
-Route::get('/ubinan-lacak', [UbinanController::class, 'showLacak'])->name('ubinan.lacak');
-Route::post('/ubinan-lacak-proses', [UbinanController::class, 'proses'])->name('ubinan.lacak.proses');
-Route::get('/ubinan-lacak-proses', [UbinanController::class, 'proses']);
-Route::post('/ubinan/petani/getBySubsegmen/{segmen}/{sub}', [PetaniController::class, 'getBySubsegmen']);
-Route::get('/ubinan/petani/getBySubsegmen/{segmen}/{sub}', [PetaniController::class, 'getBySubsegmen']);
-Route::post('/ubinan/petani/insertPetani', [PetaniController::class, 'insertPetani']);
-Route::get('/ubinan/petani/insertPetani', [PetaniController::class, 'insertPetani']);
-Route::post('/ubinan/petani/deletePetani', [PetaniController::class, 'deletePetani']);
-Route::get('/ubinan/petani/deletePetani', [PetaniController::class, 'deletePetani']);
+Route::get('/ubinan-lacak', [UbinanController::class, 'showLacak'])->middleware('auth')->name('ubinan.lacak');
+Route::post('/ubinan-lacak-proses', [UbinanController::class, 'proses'])->middleware('auth')->name('ubinan.lacak.proses');
+Route::get('/ubinan-lacak-proses', [UbinanController::class, 'proses'])->middleware('auth');
+Route::post('/ubinan/petani/getBySubsegmen/{segmen}/{sub}', [PetaniController::class, 'getBySubsegmen'])->middleware('auth');
+Route::get('/ubinan/petani/getBySubsegmen/{segmen}/{sub}', [PetaniController::class, 'getBySubsegmen'])->middleware('auth');
+Route::post('/ubinan/petani/insertPetani', [PetaniController::class, 'insertPetani'])->middleware('auth');
+Route::get('/ubinan/petani/insertPetani', [PetaniController::class, 'insertPetani'])->middleware('auth');
+Route::post('/ubinan/petani/deletePetani', [PetaniController::class, 'deletePetani'])->middleware('auth');
+Route::get('/ubinan/petani/deletePetani', [PetaniController::class, 'deletePetani'])->middleware('auth');
 
-Route::get('/ubinan-potensial', [UbinanController::class, 'showPotensial'])->name('ubinan.potensial');
+Route::get('/ubinan-potensial', [UbinanController::class, 'showPotensial'])->middleware('auth')->name('ubinan.potensial');
 
-Route::get('/ubinan-kelola', [UbinanController::class, 'showKelola'])->name('ubinan.kelola');
-Route::post('/ubinan/lacak/cadangan', [UbinanController::class, 'cadangan']);
-Route::get('/ubinan/lacak/cadangan', [UbinanController::class, 'cadangan']);
+Route::get('/ubinan-kelola', [UbinanController::class, 'showKelola'])->middleware('auth')->name('ubinan.kelola');
+Route::post('/ubinan/lacak/cadangan', [UbinanController::class, 'cadangan'])->middleware('auth');
+Route::get('/ubinan/lacak/cadangan', [UbinanController::class, 'cadangan'])->middleware('auth');
 
-Route::get('/ubinan-unggah', [UbinanController::class, 'showUnggah'])->name('ubinan.unggah');
-Route::post('/ubinan/upload', [UbinanController::class, 'import'])->name('ubinan.upload');
+Route::get('/ubinan-unggah', [UbinanController::class, 'showUnggah'])->middleware('auth')->name('ubinan.unggah');
+Route::post('/ubinan/upload', [UbinanController::class, 'import'])->middleware('auth')->name('ubinan.upload');
 
-Route::get('/ubinan-riwayat', [UbinanController::class, 'showRiwayat'])->name('ubinan.riwayat');
-Route::get('/ubinan_detail/{id}/{tahun}/{bulan}', [UbinanController::class, 'showDetail']);
+Route::get('/ubinan-riwayat', [UbinanController::class, 'showRiwayat'])->middleware('auth')->name('ubinan.riwayat');
+Route::get('/ubinan_detail/{id}/{tahun}/{bulan}', [UbinanController::class, 'showDetail'])->middleware('auth');
 
-Route::get('/ubinan-panduan', [UbinanController::class, 'showPanduan'])->name('ubinan.panduan');
+Route::get('/ubinan-panduan', [UbinanController::class, 'showPanduan'])->middleware('auth')->name('ubinan.panduan');
 
-Route::get('/ubinan/petani/getAll', [PetaniController::class, 'getAll']);
+Route::get('/ubinan/petani/getAll', [PetaniController::class, 'getAll'])->middleware('auth');
